bd3edb8d7be9c961aadaf73e161160397efdba0e
DireStorm <62968454+DireStorm@users.noreply.github.com>
2020-08-03 12:13:55 -0500

    Player Movement & Camera Follow
    In this update, I added player movement, platforms for the player to jump on, and a camera follow script  that follows the player as it moves.

---
+++ Brackey'sGameJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Variables & References
+    public Transform target;
+    public Vector3 cameraOffset;
+    public float followSpeed = 10f;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + cameraOffset;
+
+    }
+
+}
---
+++ Brackey'sGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables & References
+    public float mSpeed = 5f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    //Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += move * Time.deltaTime * mSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+    }
+}
