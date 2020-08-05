4fe3c0c9073920629491b8e5652d476bbb11119d
DireStorm <62968454+DireStorm@users.noreply.github.com>
2020-08-05 18:52:21 -0500

    Death Programming
    I set up the death and respawn basic mechanics

---
+++ Brackey'sGameJam/Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
